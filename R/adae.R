b953d22b65ec31b1e087a61440738df41a5f75c7
XunyLi37 <lixyou37@gmail.com>
2023-12-19 13:23:53 -0500

    push adae dataset

---
+++ R/adae.R
@@ -0,0 +1,9 @@
+#' Dataset adae
+#' 
+#' @format import original dataframe from adae
+#' 
+#' @source {this is a test file uploading adae dataset to the package}
+#' 
+#' @examples
+#' data(adae)
+"adae"
